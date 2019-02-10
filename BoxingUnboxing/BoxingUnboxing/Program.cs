a40464f013a07660f67f74c1c4b5c0f0046ef0c1
saraqgdeveloper <47433712+saraqgdeveloper@users.noreply.github.com>
2019-02-11 00:34:07 +0100

    Ejemplo de Boxing y Unboxing.

---
+++ BoxingUnboxing/BoxingUnboxing/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BoxingUnboxing
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Ejemplo de Boxing (Implícito)
+
+            int numero = 40;
+            object obj = numero;       
+            
+            //Ejemplo de Unboxing (Explícito)
+
+            int i = (int)obj;
+
+        }
+    }
+}
